12b2ca318d36d423262b4fe5231bdb69fda4ce0f
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:19 +0100

    Code style fix

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -3,12 +3,12 @@ declare(strict_types = 1);
 
 namespace Middlewares;
 
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Middlewares\Utils\RequestHandlerContainer;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use RuntimeException;
 
 class RequestHandler implements MiddlewareInterface
