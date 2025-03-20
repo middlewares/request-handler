72e7e7d9ff2eace7757127570885601c257a78d9
Filis Futsarov <filisfutsarov@gmail.com>
2025-03-20 12:54:12 +0100

    support for PHP 8.4 amongst other improvements

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -32,7 +32,7 @@ class RequestHandler implements MiddlewareInterface
     /**
      * Set the resolver instance.
      */
-    public function __construct(ContainerInterface $container = null)
+    public function __construct(?ContainerInterface $container = null)
     {
         $this->container = $container ?: new RequestHandlerContainer();
     }
